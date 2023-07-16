2f83eb4f3f64120aa4891a7e5a56dd15e7dd1bb9
binitkumarshirsath <binitkumar0911@gmail.com>
2023-07-16 21:03:32 +0530

    dkhmm

--- server/server.js
+++ server/server.js
@@ -2,31 +2,35 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDb from './db/db.js';
 import authRoute from './routes/auth.js';
-import categoryRoute from './routes/category.js'
-import productRoute from './routes/product.js'
-import paymentRoute from './routes/payment.js'
-import orderRoute from './routes/order.js'
-import cors from 'cors'
+import categoryRoute from './routes/category.js';
+import productRoute from './routes/product.js';
+import paymentRoute from './routes/payment.js';
+import orderRoute from './routes/order.js';
+import cors from 'cors';
 import Product from './model/productModel.js';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 
 dotenv.config();
 connectDb();
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname,'../client/build')))
+app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.use('/api',authRoute);
-app.use('/api/product',productRoute);
-app.use('/api/category',categoryRoute);
-app.use('/api',paymentRoute);
-app.use('/api/order',orderRoute);
+app.use('/api', authRoute);
+app.use('/api/product', productRoute);
+app.use('/api/category', categoryRoute);
+app.use('/api', paymentRoute);
+app.use('/api/order', orderRoute);
 
-app.use('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../client/build/index.html'))
-})
+// app.use('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
-app.listen(process.env.PORT,()=>{
-    console.log("Server is up and running at PORT : "+process.env.PORT);
-})
+app.listen(process.env.PORT, () => {
+  console.log('Server is up and running at PORT: ' + process.env.PORT);
+});
